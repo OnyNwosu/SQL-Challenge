016a6e5e07724b966fb87bcdebb2758dd9c4eb61
OnyNwosu <onuwa.nwosu@gmail.com>
2021-04-10 15:26:41 -0500

    tables

--- EmployeeSQL/Queries.sql
+++ EmployeeSQL/Queries.sql
@@ -1,3 +1,11 @@
 SELECT * FROM "Titles";
 
 SELECT * FROM "Employees";
+
+SELECT * FROM "Department_Employee";
+
+SELECT * FROM "Department_Manager";
+
+SELECT * FROM "Departments";
+
+SELECT * FROM "Salaries";
